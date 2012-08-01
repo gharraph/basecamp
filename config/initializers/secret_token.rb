695dceb5fee5942b1a9b8bba57633207483dde7e
Apprentice <apprentice@dbc08.local>
2012-08-01 10:16:53 -0700

    first commit woot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basecamp::Application.config.secret_token = 'bc8f654c5ece7aa9529b3636739cc8bf55ab9b18caa5b90d3339744d40db10be800009aaa5133db6015916653836cd79d2868f22a20ec2afc5dbe454b121085d'
